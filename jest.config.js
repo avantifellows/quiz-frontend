95a2b2f2bc2f717091998467ad3ee2ca53450de4
Aman Dalmia <amandalmia18@gmail.com>
2022-02-24 10:10:14 +0530

    NEW: added a valid unit test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  preset: "@vue/cli-plugin-unit-jest",
   transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    "^.+\\.vue$": "@vue/vue3-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!src/main.js",
+    "!src/**/dist/*.js",
+  ],
+  setupFiles: ["<rootDir>/jest.init.js"],
+};
