76acf485913d4b6540dab2b99f8fcc1c6c586812
Aman Dalmia <amandalmia18@gmail.com>
2022-02-25 15:17:54 +0530

    ENH: move player.vue and header.vue to composition api

--- src/components/Items/Header.vue
+++ src/components/Items/Header.vue
@@ -4,8 +4,8 @@
     <icon-button
       v-if="isSkipEnabled"
       :titleConfig="skipButtonTitleConfig"
-      @click="skipItem"
       :buttonClass="skipButtonClass"
+      @click="skipItem"
       data-test="skip"
     ></icon-button>
   </div>
@@ -13,36 +13,45 @@
 
 <script>
 import IconButton from '@/components/UI/Buttons/IconButton.vue'
+import { reactive, toRefs, computed } from 'vue'
 
 export default {
   name: 'Header',
-  data() {
-    return {
+  setup(props, context) {
+    const data = reactive({
       // styling class for the skip button
       skipButtonClass:
         'bg-primary hover:bg-primary-hover p-1 pl-4 pr-4 sm:p-2 sm:pl-10 sm:pr-10 lg:p-4 lg:pl-10 lg:pr-10 rounded-md shadow-xl disabled:opacity-50 disabled:pointer-events-none h-full'
+    })
+    function skipItem() {
+      context.emit('skip-item')
     }
-  },
-  components: { IconButton },
-  computed: {
-    /** main styling class for this component */
-    containerClass() {
+    // main styling class for this component
+    const containerClass = computed(() => {
       return [
         {
-          'px-6 md:px-8 xl:px-12': !this.previewMode,
-          'pr-4': this.previewMode
+          'px-6 md:px-8 xl:px-12': !props.previewMode,
+          'pr-4': props.previewMode
         },
         'flex w-full bg-white justify-end p-1 space-x-2 mt-2'
       ]
-    },
-    /** config for the title of skip button */
-    skipButtonTitleConfig() {
+    })
+    // config for the title of skip button
+    const skipButtonTitleConfig = computed(() => {
       return {
         value: 'Skip',
         class: 'text-white text-md sm:text-base lg:text-xl font-bold'
       }
+    })
+
+    return {
+      ...toRefs(data),
+      skipItem,
+      containerClass,
+      skipButtonTitleConfig
     }
   },
+  components: { IconButton },
   props: {
     /** whether the item modal will be shown in editor's mini-preview mode */
     previewMode: {
@@ -52,11 +61,6 @@ export default {
     isSkipEnabled: {
       default: true,
       type: Boolean
-    }
-  },
-  methods: {
-    skipItem() {
-      this.$emit('skip-item')
     }
   },
   emits: ['skip-item']
--- src/components/UI/Buttons/IconButton.vue
+++ src/components/UI/Buttons/IconButton.vue
@@ -37,7 +37,6 @@ export default {
       }
     }
   },
-  created() {},
   props: {
     iconConfig: {
       type: Object,
