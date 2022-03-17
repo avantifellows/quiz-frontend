036e08374ebc650693dba3c40d42f9e141444612
SaloniChoudhary <salonichoudhary1999@gmail.com>
2022-03-17 13:23:58 +0530

    e2e tests updated

--- tests/e2e/specs/renders-player.js
+++ tests/e2e/specs/renders-player.js
@@ -1,14 +1,18 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("Player", () => {
-  it("shows splash screen", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+  it("shows splash screen", () => {
     cy.get('[data-test="splash"]').should("exist");
     cy.get('[data-test="header"]').should("not.exist");
+    cy.get('[data-test="startQuiz"]').should("exist");
   });
 
-  it("shows start button on splash screen", () => {
-    cy.visit("/");
-    cy.get('[data-test="startQuiz"]').should("exist");
+  it("on click of start button on splash screen", () => {
+    cy.get('[data-test="startQuiz"]').trigger("click");
+    cy.get('[data-test="splash"]').should("not.exist");
+    cy.get('[data-test="header"]').should("exist");
   });
 });
