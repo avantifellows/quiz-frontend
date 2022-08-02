a0994ef9aa8a9be431d3e6395fb7c4e5a9945c28
suryabulusu <teja.surya59@gmail.com>
2022-08-02 20:52:37 +0530

    adds krutidev hindi font

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 </template>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "KrutiDev", Avenir, Helvetica, Arial, sans-serif;
+}
+@font-face {
+  font-family: "Kruti Dev";
+  src: local("Kruti Dev"), url("./assets/fonts/Kruti_Dev_10.TTF") format("truetype");
 }
 </style>
