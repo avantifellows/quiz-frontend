27999ee75ad7b4c858a35e27752f94557fb41a0b
suryabulusu <teja.surya59@gmail.com>
2022-10-13 23:49:44 +0530

    removes console log

--- src/services/API/FormInputHandling.ts
+++ src/services/API/FormInputHandling.ts
@@ -6,7 +6,6 @@ export default {
   async submitFormData(formData : any, resultsQuery: boolean = false): Promise<FormResultResponse> {
     const lambdaUrl = process.env.VUE_APP_FORM_LAMBDA;
     formData.results_query = resultsQuery;
-    console.log(resultsQuery);
 
     const response = await fetch(lambdaUrl, {
       method: 'POST',
