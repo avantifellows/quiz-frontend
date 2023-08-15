d66e6133380d01de4c15901bd5ffa721f9c2c2ad
suryabulusu <teja.surya59@gmail.com>
2023-08-15 13:57:50 +0530

    removes delay

--- src/services/API/RootClient.ts
+++ src/services/API/RootClient.ts
@@ -5,20 +5,9 @@ import ErrorHandling from "./ErrorHandling";
 const client = axios.create({
   baseURL: process.env.VUE_APP_BACKEND,
   withCredentials: false,
-  timeout: 4000, // cancel request after 10 sec
+  timeout: 4000, // cancel request after 4 sec
   // but this works only when internet connection is active
   // ref: https://stackoverflow.com/questions/36690451/timeout-feature-in-the-axios-library-is-not-working
-});
-
-// delay response by one second to notice loading spinner
-client.interceptors.response.use(function (response) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(response);
-    }, 1000);
-  });
-}, function (error) {
-  return Promise.reject(error);
 });
 
 // handle errors in responses for API requests
