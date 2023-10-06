163840c5a8cac43fa94be99658e60baf39661f5f
suryabulusu <teja.surya59@gmail.com>
2023-10-06 17:23:04 +0530

    removes thread loader and updates vue config

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,8 @@ module.exports = {
     // reference: https://github.com/vuejs/vue-cli/issues/979#issuecomment-372990631
     config.plugins.delete("prefetch");
   },
+  parallel: false,
+  // https://www.npmjs.com/package/@vue/cli-plugin-babel
+  // `thread-loader` is enabled by default when the machine has more than 1 CPU cores.
+  // This can be turned off by setting parallel: false in vue.config.js
 };
