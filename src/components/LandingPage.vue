8e5a53bcbf30594fa99fd9615e2050b683d06547
Arya <root@LAPTOP-ES1GMUTU>
2023-10-11 19:57:10 +0530

    Home Page for Quiz Engine

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="grid h-screen place-items-center">
+      <p class="text-5xl">Welcome to AF Quiz Engine</p>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "LandingPage",
+};
+</script>
